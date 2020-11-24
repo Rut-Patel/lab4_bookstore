97646b0cc81c0cd7d72e474d26f17149cce74039
Rut Patel <patelrut238@gmail.com>
2020-11-23 20:23:44 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using lab4_bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab4_bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Appraise()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Appraise(textbook model)
+        {
+            string message = "";
+
+            if(ModelState.IsValid)
+            {
+                ViewData["Message"] = "Your textbook: " + model.title + " ,version: " + model.version;
+                ViewData["Appraise"] = "was appraised at: $" + model.appraisalPrice(model.price,model.condition);
+
+                return View("Appraised", model);
+            }
+            else
+            {
+                //message = "Failed to appraise the price. Please try again.";
+                return View("Error");
+            }
+
+           
+        }
+    }
+}
---
+++ Models/textbook.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab4_bookstore.Models
+{
+    public class textbook
+    {
+        public string title { get; set; }
+        public int ISBN { get; set; }
+        public string version { get; set; }
+        public double price { get; set; }
+        public string condition { get; set; }
+
+        public textbook()
+        {
+
+        }
+
+        public textbook(string title,int ISBN, string version,double price,string condition)
+        {
+            this.title = title;
+            this.ISBN = ISBN;
+            this.version = version;
+            this.price = price;
+            this.condition = condition;
+        }
+
+        public double appraisalPrice(double price,string condition)
+        {
+            double finalPrice;
+
+           if(condition == "Bad")
+           {
+                finalPrice = price / 4;
+           }
+           else if(condition == "Good")
+           {
+                finalPrice = price / 3;
+           }
+           else
+           {
+                finalPrice = price / 2;
+           }
+           
+            return Math.Round(finalPrice,2);
+        }
+
+        public override string ToString()
+        {
+            return "Hello, your book: " + this.title + " version: " + this.version + " and with ISBN: " + this.ISBN + " can be appraised at: $" + appraisalPrice(price,condition) ;
+        }
+    }
+}
